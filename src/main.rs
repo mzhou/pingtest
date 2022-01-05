7afbde20541fda791b7788fd18b086b8505f9fc2
Michael Zhou <mzhou@cse.unsw.edu.au>
2022-01-06 03:12:32 +1100

    add request based route

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
+#![feature(async_closure)]
+
 use std::time::{Duration, Instant};
 
 use async_stream::stream;
+use bytes::Bytes;
 use clap::{App, Arg};
 use failure_derive::Fail;
-use futures::Stream;
 use futures::sink::SinkExt;
 use futures::stream::StreamExt;
+use futures::Stream;
 use serde_json::json;
-use warp::Filter;
 use warp::http::header::{HeaderMap, HeaderValue};
 use warp::ws::{WebSocket, Ws};
+use warp::Filter;
 
 type ResultFailure<O> = Result<O, failure::Error>;
 
 const INDEX_HTML_BYTES: &[u8] = include_bytes!("../static/index.html");
 const JITTER_HTML_BYTES: &[u8] = include_bytes!("../static/jitter.html");
+const LOOP_HTML_BYTES: &[u8] = include_bytes!("../static/loop.html");
 const LISTEN_ADDR: &str = "LISTEN_ADDR";
 
 #[tokio::main]
@@ -45,9 +49,13 @@ async fn main() -> ResultFailure<()> {
 
     let jitter_sse_route = warp::path("jitter.sse")
         .and(warp::get())
-        .map(|| {
-            warp::sse::reply(warp::sse::keep_alive().stream(jitter_sse_events()))
-        });
+        .map(|| warp::sse::reply(warp::sse::keep_alive().stream(jitter_sse_events())));
+
+    let echo_route = warp::path("echo")
+        .and(warp::post())
+        .and(warp::filters::body::content_length_limit(64 * 1024 - 1))
+        .and(warp::filters::body::bytes())
+        .map(handle_echo);
 
     let html_page = |b| {
         let mut headers = HeaderMap::new();
@@ -59,17 +67,33 @@ async fn main() -> ResultFailure<()> {
             "Cross-Origin-Opener-Policy",
             HeaderValue::from_static("same-origin"),
         );
-        warp::any().map(move || warp::http::Response::builder().body(b)).with(warp::reply::with::headers(headers))
+        warp::any()
+            .map(move || warp::http::Response::builder().body(b))
+            .with(warp::reply::with::headers(headers))
     };
 
     let jitter_html_route = warp::path("jitter.html").and(html_page(JITTER_HTML_BYTES));
+    let loop_html_route = warp::path("loop.html").and(html_page(LOOP_HTML_BYTES));
     let index_html_route = warp::path::end().and(html_page(INDEX_HTML_BYTES));
 
-    let routes = ws_route.or(ws2_route).or(jitter_sse_route).or(jitter_html_route).or(index_html_route);
+    let routes = ws_route
+        .or(echo_route)
+        .or(jitter_html_route)
+        .or(jitter_sse_route)
+        .or(loop_html_route)
+        .or(ws2_route)
+        .or(index_html_route);
 
     warp::serve(routes).run(listen_addr).await;
 
     Ok(())
+}
+
+fn handle_echo(b: Bytes) -> Result<warp::http::Response<Bytes>, warp::http::Error> {
+    warp::http::Response::builder()
+        .status(200)
+        .header("Content-Type", "application/octet-stream")
+        .body(b)
 }
 
 async fn handle_ws_void(sock: WebSocket) -> () {
@@ -93,7 +117,8 @@ async fn handle_ws2_void(sock: WebSocket) -> () {
 
 async fn handle_ws2(mut sock: WebSocket) -> ResultFailure<()> {
     let first_msg = sock.next().await.ok_or(EndOfStreamError {})??;
-    if b"\x01" == first_msg.as_bytes() { // jitter 50 ms
+    if b"\x01" == first_msg.as_bytes() {
+        // jitter 50 ms
         let start_instant = Instant::now();
         loop {
             let instant = Instant::now();
